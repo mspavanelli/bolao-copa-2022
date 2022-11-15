9696296f7dc5a292a33925c68f90d29df3db0474
Matheus Pavanelli <mspavanelli@gmail.com>
2022-11-15 01:52:43 -0300

    feat: disable mobile zoom

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
