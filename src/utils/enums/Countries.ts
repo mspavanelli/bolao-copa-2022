86633f10e4ee87ee78c9595d741e9bcc9c41d396
Matheus Pavanelli <mspavanelli@gmail.com>
2022-11-21 22:26:04 -0300

    styles: add countries spaces between groups

--- src/utils/enums/Countries.ts
+++ src/utils/enums/Countries.ts
@@ -3,30 +3,37 @@ export const Countries = {
   EC: "EC",
   SN: "SN",
   NL: "NL",
+
   "GB-ENG": "GB-ENG",
   IR: "IR",
   US: "US",
   "GB-WLS": "GB-WLS",
+
   AR: "AR",
   SA: "SA",
   MX: "MX",
   PL: "PL",
+
   FR: "FR",
   AU: "AU",
   DK: "DK",
   TN: "TN",
+
   ES: "ES",
   CR: "CR",
   DE: "DE",
   JP: "JP",
+
   BE: "BE",
   CA: "CA",
   MA: "MA",
   HR: "HR",
+
   BR: "BR",
   RS: "RS",
   CH: "CH",
   CM: "CM",
+
   PT: "PT",
   GH: "GH",
   UY: "UY",
