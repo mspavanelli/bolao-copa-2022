375b9683973ab70c512a8302b3fa5b379e4d558a
Matheus Pavanelli <mspavanelli@gmail.com>
2022-11-22 01:11:35 -0300

    feat: login with google

--- src/components/views/Login/index.tsx
+++ src/components/views/Login/index.tsx
@@ -1,7 +1,28 @@
-import { Button } from "@/components/widgets/atoms/Button";
-import Link from "next/link";
+import { signInWithPopup, GoogleAuthProvider, getAuth } from "firebase/auth";
+import { useRouter } from "next/router";
+
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+auth.languageCode = "pt-br";
 
 export function Login() {
+  const router = useRouter();
+
+  async function handleLogin() {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential?.accessToken;
+      const { user } = result;
+      const { displayName, photoURL } = user;
+      alert({ displayName });
+
+      router.push("/dashboard");
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <div className=" flex h-screen flex-col items-center justify-between bg-red-900 p-8">
       <div />
@@ -11,11 +32,12 @@ export function Login() {
         Qatar 2022
       </h1>
 
-      <Button>
-        <Link href="/dashboard" className=" w-full">
-          <a>Login</a>
-        </Link>
-      </Button>
+      <button
+        className="w-full rounded bg-yellow-300 p-3 font-semibold text-stone-800 transition active:opacity-80"
+        onClick={handleLogin}
+      >
+        Login
+      </button>
     </div>
   );
 }
