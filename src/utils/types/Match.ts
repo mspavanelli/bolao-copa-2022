216a7e3ef69721ded89ff43f310bb1e84f547695
Matheus Pavanelli <mspavanelli@gmail.com>
2022-11-21 22:25:30 -0300

    feat: update match type fieds

--- src/utils/types/Match.ts
+++ src/utils/types/Match.ts
@@ -1,12 +1,12 @@
 import { MatchStatus } from "@/utils/enums/MatchStatus";
 
 export type Match = {
-  id: string;
+  id?: string;
   date: Date;
   weight: number;
   first_team: string;
   first_team_score: number;
   second_team: string;
   second_team_score: number;
-  status: MatchStatus;
+  status?: MatchStatus;
 };
